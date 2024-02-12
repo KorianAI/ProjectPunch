d6dc253f6878fbbfbb5f06470f6a9060617b3a77
Ian O'Flaherty <ioflaherty14@hotmail.com>
2024-02-12 22:19:53 +0000

    fixed the not rotating on joystick stuff

--- Assets/InputMaster.cs
+++ Assets/InputMaster.cs
@@ -141,7 +141,7 @@ public partial class @InputMaster: IInputActionCollection2, IDisposable
                 {
                     ""name"": ""Left Stick"",
                     ""id"": ""d4f5bf9a-5787-44a8-8095-bf558064cfbf"",
-                    ""path"": ""2DVector"",
+                    ""path"": ""2DVector(mode=2)"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
