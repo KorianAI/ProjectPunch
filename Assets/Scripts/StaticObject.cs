7a356e01e8126ed2bea3dbbbc443448cd76eae60
14cuttingj <14cuttingj@gmail.com>
2024-03-18 16:38:48 +0000

    new spotlights

--- Assets/Scripts/StaticObject.cs
+++ Assets/Scripts/StaticObject.cs
@@ -6,19 +6,16 @@ using DG.Tweening;
 public class StaticObject : MonoBehaviour, IMagnetisable
 {
     public GameObject playerObj;
-    public GameObject playerCam;
     public Transform pullPos;
 
     private void Start()
     {
-        //playerObj = GameObject.Find("Player");
-        //playerCam = GameObject.Find("ThirdPersonCamera");
+        playerObj = GameObject.Find("Player");
     }
 
     public void Pull(PlayerStateManager player)
     {
         playerObj.transform.DOMove(pullPos.transform.position, 1.5f);
-        //playerCam.transform.DOMove(pullPos.transform.position, 1f);
     }
 
     public void Push(PlayerStateManager player)
