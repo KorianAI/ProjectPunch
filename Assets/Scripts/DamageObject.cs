2cdaf75d9c2a03743c603ac491623d8315b56382
KorianAI <koriannnai@gmail.com>
2024-04-19 15:36:49 +0100

    homing stuff back

--- Assets/Scripts/DamageObject.cs
+++ Assets/Scripts/DamageObject.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,11 +8,17 @@ public class DamageObject : MonoBehaviour
 {
     public int damage;
     public GameObject hitVFX;
+
     
+    public bool extendoFists;
+    public GameObject projectile;
+
+    public bool whip;
+    public LayerMask enemyLayer;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
         var target = other.gameObject.GetComponent<IDamageable>();
 
         if (target != null && !other.gameObject.CompareTag("Player"))
@@ -22,7 +29,44 @@ public class DamageObject : MonoBehaviour
             if (other.gameObject.tag == "Enemy") // check in place to avoid errors when hitting scrap piles -J
                 other.gameObject.GetComponent<EnemyHealth>().GetStunned(.1f);
 
-            IncreaseShotgunAmmo();
+            if (whip)
+            {
+                IncreaseShotgunAmmo();
+            }
+
+            if (extendoFists)
+            {
+                LaunchProjectile(other.transform, other.gameObject);
+            }
+          
+        }
+    }
+
+    void LaunchProjectile(Transform hitPos, GameObject hitEnemy)
+    {
+        Collider[] colliders = Physics.OverlapSphere(hitPos.position, 5, enemyLayer);
+        colliders = RemoveHitEnemy(colliders, hitEnemy);
+
+        Debug.Log(hitEnemy + "1");
+
+        if (colliders.Length > 0)
+        {
+            // Choose a random enemy from the list
+            GameObject randomEnemy = colliders[Random.Range(0, colliders.Length)].gameObject;
+            Debug.Log(randomEnemy + "2");
+
+            // Instantiate projectile at the hit point
+            GameObject proj = Instantiate(projectile, hitPos.position, Quaternion.identity);
+            HomingProjectile h = proj.GetComponent<HomingProjectile>();
+
+            // Calculate direction towards the random enemy
+            Vector3 targetPosition = randomEnemy.transform.position;
+
+            // Use DOTween to move the projectile towards the target
+            proj.transform.DOMove(targetPosition, .3f)
+                .SetEase(Ease.Linear)
+                .OnComplete(() => h.Detonate()); // Destroy the projectile when the tween is complete
+
         }
     }
 
@@ -35,5 +79,23 @@ public class DamageObject : MonoBehaviour
             Debug.Log("uwu");
             shotgun.ChangeAmmo(1);
         }
+    }
+
+    Collider[] RemoveHitEnemy(Collider[] colliders, GameObject hitEnemy)
+    {
+        // Create a list to store the filtered colliders
+        var filteredColliders = new List<Collider>();
+
+        // Iterate through the colliders and add them to the list if they're not the hit enemy
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject != hitEnemy)
+            {
+                filteredColliders.Add(collider);
+            }
+        }
+
+        // Convert the list back to an array
+        return filteredColliders.ToArray();
     }
 }
---
+++ Assets/Scripts/HomingProjectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingProjectile : MonoBehaviour
+{
+    public LayerMask enemyLayer;
+    public GameObject explosionVFX;
+
+    public void Detonate()
+    {
+        GameObject hitParticle = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+
+        Collider[] enemies = Physics.OverlapSphere(transform.position, 2, enemyLayer);
+        foreach (Collider c in enemies)
+        {
+            c.GetComponent<IDamageable>().TakeDamage(10);         
+            //RumbleManager.instance.RumblePulse(.25f, 1f, .25f);
+        }
+
+        Debug.Log("lol");
+        Destroy(gameObject);
+    }
+}
