f6c9de07960b5486c0f1d40d5f9a922002abdd8a
14cuttingj <14cuttingj@gmail.com>
2024-05-01 15:42:50 +0100

    few bits

--- Assets/Scripts/AnimationEventTrigger.cs
+++ Assets/Scripts/AnimationEventTrigger.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class AnimationEventTrigger : MonoBehaviour
 {
     [SerializeField] ExtendoArms extArms;
+    [SerializeField] ParticleSystem dustParticle;
+
+
     public void TriggerShockwave(float value)
     {
         if (extArms != null)
         {
             extArms.Shockwave(value);
         }
+    }
+
+    public void DustPlay()
+    {
+        dustParticle.Play();
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,14 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject loading;
+    
     private void Start()
     {
-
+        loading.SetActive(false);
     }
 
     public void ChangeScene(int sceneId)
     {
         //make the screen fade to black first?
+
+        loading.SetActive(true);
         
         SceneManager.LoadScene(sceneId);
     }    
