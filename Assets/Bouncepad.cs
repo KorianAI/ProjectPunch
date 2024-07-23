9aecb29d40490c59488d5fde012bafb6c0917cf8
14cuttingj <14cuttingj@gmail.com>
2024-07-23 08:39:56 +0100

    Trying to make stuff happen

--- Assets/Bouncepad.cs
+++ Assets/Bouncepad.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Cinemachine;
+using Dreamteck.Splines;
 
 public class Bouncepad : MonoBehaviour, IMagnetisable
 {
+    PlayerStateManager ps;
+    public SplineComputer flipSpline;
+
     public void Pull(PlayerStateManager player)
     {
         
@@ -11,6 +17,17 @@ public class Bouncepad : MonoBehaviour, IMagnetisable
 
     public void Push(PlayerStateManager player)
     {
-        player.pm.JumpForce();
+        //player.pm.JumpForce();
+
+        player.splineFollower.enabled = true;
+        player.splineFollower.spline = flipSpline;
+        player.splineFollower.Restart();
+
+        //speed lines, pull out cam?
     }
+
+    //if player is standing on the pad and presses push
+    //activate the spline follower
+    //make the player move along the curve of the spline
+    
 }
