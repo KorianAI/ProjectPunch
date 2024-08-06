5d9786ef6bd534763c3aaf7a1bb876f284e294a5
Ian O'Flaherty <ioflaherty14@hotmail.com>
2024-08-06 23:31:39 +0100

    Bit of nail juice

--- Assets/ConcentratedNail.cs
+++ Assets/ConcentratedNail.cs
@@ -15,7 +15,11 @@ public class ConcentratedNail : MonoBehaviour, IMagnetisable, IParriable
     public Transform spawnPoint;
     public PlayerStateManager sm;
 
-
+    public GameObject explosionPrefab;
+    public float explosionRange;
+    public float explosionDamage;
+    public LayerMask enemyLayer;
+    public AttackStats explosionStats;
 
     private void Start()
     {
@@ -51,6 +55,29 @@ public class ConcentratedNail : MonoBehaviour, IMagnetisable, IParriable
 
     }
 
+    public void Detonate()
+    {
+        GameObject vfx = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Collider[] enemies = Physics.OverlapSphere(transform.position, 4f, enemyLayer);
+        foreach (Collider c in enemies)
+        {
+            c.GetComponent<IDamageable>().TakeDamage(explosionDamage);
+
+            HitstopManager.Instance.TriggerHitstop(explosionStats.hitstopAmnt, gameObject, c.gameObject);
+            CinemachineShake.Instance.ShakeCamera(explosionStats.shakeAmnt, explosionStats.shakeAmnt);
+            CinemachineShake.Instance.ChangeFov(explosionStats.zoomAmnt, explosionStats.shakeDur);
+            RumbleManager.instance.RumblePulse(.15f, .25f, .3f);
+
+            if (c.GetComponent<EnemyHealth>() != null)
+            {
+                c.GetComponent<EnemyHealth>().GetStunned(.2f);
+
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
     public void ReturnToSpawn()
     {
         Vector3 direction = destination.position - sm.transform.position;
@@ -66,6 +93,6 @@ public class ConcentratedNail : MonoBehaviour, IMagnetisable, IParriable
         Debug.Log("uwaahhhh parried!");
         float distance = Vector3.Distance(spawnPoint.position, destination.position);
         float dur = distance / speed;
-        transform.DOMove(destination.position, dur).OnComplete(DestroyNail);
+        transform.DOMove(destination.position, dur).OnComplete(Detonate);
     }
 }
