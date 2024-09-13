1a65a7e8c720410c30ac581cd731ed48c53c3ba2
ADAMANT1UM97 <adamfjackson03@gmail.com>
2024-09-13 23:49:33 +0100

    MORE MORE (AdamsSFXDungeon)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,33 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MUSICEND_COMBATOUTRO = 3854394445U;
+        static const AkUniqueID MUSICEND_EXPLORE = 3473329445U;
+        static const AkUniqueID MUSICPLAY_COMBATMAIN = 3273142224U;
+        static const AkUniqueID MUSICPLAY_EXPLORE = 4048598968U;
         static const AkUniqueID SFXPLAY_FOOTSTEPSGROUND = 3800919789U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATES
+        {
+            static const AkUniqueID GROUP = 1690668539U;
+
+            namespace STATE
+            {
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID EXPLORE = 579523862U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace MUSIC_STATES
+
+    } // namespace STATES
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID ENDCOMBAT = 2627423420U;
+    } // namespace TRIGGERS
 
     namespace BANKS
     {
@@ -24,8 +49,11 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID COMBAT_1_SECTION_A_MUSIC_BUS = 4083411863U;
+        static const AkUniqueID COMBAT_1_SECTION_B_MUSIC_BUS = 2199471764U;
+        static const AkUniqueID EXPLORE_MUSIC_BUS = 1570563853U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID MASTER_MUSIC_BUS = 48433064U;
         static const AkUniqueID SFX_BUS = 1502772432U;
     } // namespace BUSSES
 
