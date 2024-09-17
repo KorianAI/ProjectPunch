9e58a108f7fb2992d380fdb6ffcb1d84fb97f7fc
ADAMANT1UM97 <adamfjackson03@gmail.com>
2024-09-17 20:43:16 +0100

    More Kaila SFX - AdamsSFXDungeon (sorry in advance for how loud they are)

--- Assets/SFX/SFX Scripts/PlayerSFX_Overview.cs
+++ Assets/SFX/SFX Scripts/PlayerSFX_Overview.cs
@@ -8,11 +8,25 @@ using AK.Wwise.Editor;
 public class PlayerSFX_Overview : MonoBehaviour
 {
     PlayerResources pr;
+    [Header("Movement")]
+    public AK.Wwise.Event playSFX_footsteps_ground;
+    public AK.Wwise.Event playSFX_footsteps_metal;
+    public AK.Wwise.Event playSFX_dash;
+    public AK.Wwise.Event playSFX_RS_dash;
+    public AK.Wwise.Event playSFX_jump;
+    public AK.Wwise.Event playSFX_land;
+    public AK.Wwise.Event playSFX_launch;
+
     [Header("BFG")]
     public AK.Wwise.Event playSFX_BFG_impact;
     public AK.Wwise.Event playSFX_BFG_slam;
     public AK.Wwise.Event playSFX_BFG_swing;
     public AK.Wwise.Event playSFX_BFG_lunge;
+
+    [Header("Nail Gun")]
+    public AK.Wwise.Event playSFX_NailG_burst;
+    public AK.Wwise.Event playSFX_NailG_charged;
+    public AK.Wwise.Event playSFX_NailG_chargedpull;
 
     [Header("Real Steel")]
     public AK.Wwise.Event playSFX_RS_impact;
@@ -22,11 +36,30 @@ public class PlayerSFX_Overview : MonoBehaviour
     public AK.Wwise.Event playSFX_RS_overdriveactive;
     public AK.Wwise.Event playSFX_RS_overdriveoutro;
 
+    bool overdriveActive = false;
+    bool overdriveOutroPlayed = true;
+
 
     private void Start()
     {
         pr = GetComponent<PlayerResources>();
     }
+
+    //private void Update()
+    //{
+    //    if (pr.shift.overdrive)
+    //    {
+    //        overdriveActive = true;
+    //        overdriveOutroPlayed = false;
+    //        SFX_RS_Overdrive();
+    //    }
+
+    //    else
+    //    {
+    //        overdriveActive = false;
+    //        SFX_RS_Overdrive();
+    //    }
+    //}
 
     public void SFX_Swing()
     {
@@ -83,8 +116,52 @@ public class PlayerSFX_Overview : MonoBehaviour
 
         else
         {
-            playSFX_RS_overdriveoutro.Post(gameObject);
+            if (overdriveActive == false && overdriveOutroPlayed == false)
+            {
+                playSFX_RS_overdriveoutro.Post(gameObject);
+                overdriveOutroPlayed = true;
+            }
+           
         }
 
+    }
+
+    public void SFX_NailG_Burst()
+    {
+        playSFX_NailG_burst.Post(gameObject);
+    }
+
+    public void SFX_NailG_Charged()
+    {
+        playSFX_NailG_charged.Post(gameObject);
+    }
+
+    public void SFX_NailG_ChargedPull()
+    {
+        playSFX_NailG_chargedpull.Post(gameObject);
+    }
+
+    public void SFX_Footsteps()
+    {
+        playSFX_footsteps_ground.Post(gameObject);
+    }
+
+    public void SFX_Dash()
+    {
+        if (pr.scrapShift)
+        {
+            playSFX_RS_dash.Post(gameObject);
+        }
+
+        else
+        {
+            playSFX_dash.Post(gameObject);
+            Debug.Log("Playing sound");
+        }
+    }
+
+    public void SFX_BouncePad_Launch()
+    {
+        playSFX_launch.Post(gameObject);
     }
 }
