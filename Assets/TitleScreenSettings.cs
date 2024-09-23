9bff966bdd027dae41a481d746ace5d3df922f2a
14cuttingj <14cuttingj@gmail.com>
2024-09-23 18:32:02 +0100

    tuts and startup

--- Assets/TitleScreenSettings.cs
+++ Assets/TitleScreenSettings.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Video;
 
 public class TitleScreenSettings : MonoBehaviour
 {
     [Header("Intro")]
+    public VideoPlayer vp;
     public GameObject introVidObj;
+    public float timeBefore;
+    public GameObject blackScreen;
 
     [Header("Toggle Settings")]
     public GameObject settingsUI;
     public GameObject mainUI;
     public bool paused;
     public PauseTabGroup tabGroup;
+
+    private void Awake()
+    {
+        //play music here
+
+        if (introVidObj != null)
+        {
+            blackScreen.SetActive(true);
+            vp.enabled = false;
+            StartCoroutine(StartVideo());
+        }
+
+        if (settingsUI != null)
+        {
+            settingsUI.SetActive(false);
+        }
+
+        if (mainUI != null)
+        {
+            mainUI.SetActive(true);
+        }
+    }
 
     private void Start()
     {
@@ -25,28 +51,6 @@ public class TitleScreenSettings : MonoBehaviour
         {
             PauseToggle();
         };
-
-
-    }
-
-    private void Awake()
-    {
-        //play music here
-
-        if (introVidObj != null)
-        {
-            introVidObj.SetActive(true);
-        }
-
-        if (settingsUI != null)
-        {
-            settingsUI.SetActive(false);
-        }
-
-        if (mainUI != null)
-        {
-            mainUI.SetActive(true);
-        }
     }
 
     public void ToggleUI(GameObject ui)
@@ -105,7 +109,15 @@ public class TitleScreenSettings : MonoBehaviour
 
         tabGroup.defaultTab.Select(); //set tab group to page 1
 
-
         InputMapManager.ToggleActionMap(InputMapManager.inputActions.Player);
+    }
+
+    IEnumerator StartVideo()
+    {
+        yield return new WaitForSeconds(timeBefore);
+        introVidObj.SetActive(true);
+        vp.enabled = true;
+        yield return new WaitForSeconds(1f);
+        blackScreen.SetActive(false);
     }
 }
