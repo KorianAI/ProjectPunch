2877f78f61d6a7bd09fd26a079eab4876af7c552
ADAMANT1UM97 <adamfjackson03@gmail.com>
2024-09-24 05:20:02 +0100

    Title Screen Music WIP

---
+++ Assets/TitleScreenAudio.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenAudio : MonoBehaviour
+{
+
+    [Header("Main Theme")]
+    public AK.Wwise.Event playMusic_MainTheme;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MusicStart()
+    {
+
+        Music_MainTheme();
+    }
+
+    public void Music_MainTheme()
+    {
+        playMusic_MainTheme.Post(gameObject);
+    }
+}
