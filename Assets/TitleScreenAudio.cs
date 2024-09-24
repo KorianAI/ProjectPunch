e62909d2129df8933dddf76f334d59988ac92f17
ADAMANT1UM97 <adamfjackson03@gmail.com>
2024-09-24 16:19:21 +0100

    Title Screen Music

--- Assets/TitleScreenAudio.cs
+++ Assets/TitleScreenAudio.cs
@@ -10,7 +10,7 @@ public class TitleScreenAudio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MusicStart();
+        Music_MainTheme();
     }
 
     // Update is called once per frame
